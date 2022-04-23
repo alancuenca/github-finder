0a9fc6fa285062b0abdf57e233e5d8607fe8dc92
alancuenca <alan.r.cuenca@gmail.com>
2022-04-22 23:02:04 -0700

    created new pages folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
