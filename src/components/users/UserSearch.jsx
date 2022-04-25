2d4c54b040e49a21e1715047da651dbd4d2efe56
Alan Cuenca <alan.r.cuenca@gmail.com>
2022-04-25 01:06:22 -0700

    search added more features

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,15 +1,37 @@
+import { useState, useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
+
 function UserSearch() {
+    const [text, setText] = useState('')
+
+    const { users } = useContext(GithubContext)
+
+    const handleChange = (e) => setText(e.target.value)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (text === '') {
+            alert('Please enter search')
+        } else {
+            // @todo - search users
+            setText('')
+        }
+    }
+
     return (
 
         <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form-control">
                         <div className="relative">
                             <input
                                 type='text'
                                 className="w-full pr-40 bg-gray-200 input input-lg text-black"
                                 placeholder='Search...'
+                                value={text}
+                                onChange={handleChange}
                             />
                             <button
                                 type='submit'
@@ -20,11 +42,14 @@ function UserSearch() {
                     </div>
                 </form>
             </div>
-            <div>
-                <button className="btn btn-ghost btn-lg">
-                    Clear
-                </button>
-            </div>
+            {/* only show this if there are users in the state */}
+            {users.length > 0 && (
+                <div>
+                    <button className="btn btn-ghost btn-lg">
+                        Clear
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
