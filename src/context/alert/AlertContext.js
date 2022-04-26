26093fe61b13eff22aa938b38eeb70a20af7b696
Alan Cuenca <alan.r.cuenca@gmail.com>
2022-04-25 22:16:35 -0700

    created a timeout for the alert

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -8,6 +8,15 @@ export const AlertProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(alertReducer, initialState)
 
+    // Set alert
+    const setAlert = (msg, type) => {
+        dispatch({
+            type: 'SET_ALERT',
+            payload: {msg, type}
+        })
+        setTimeout(() => dispatch({ type: 'REMOBE_ALERT'}), 3000)
+    }
+
     return <AlertContext.Provider value={{alert: state}}>
         {children}
     </AlertContext.Provider>
