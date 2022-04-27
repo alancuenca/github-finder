3afa2318a34cb013a6a0e4e5d51b4167ee19eb7b
Alan Cuenca <alan.r.cuenca@gmail.com>
2022-04-26 23:33:16 -0700

    replaced get users and get userrepos with axios

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { useResolvedPath } from "react-router-dom"
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
@@ -17,44 +18,54 @@ export const searchUsers = async (text) => {
     const response = await github.get(`/search/users?${params}`)
     return response.data.items
 }
+// ----------------------- ALL OF THIS IS NOW REPLACED WITH AXIOS
+// // Get search results & 'login' is the users' name on github
+// export const getUser = async (login) => {
 
-// Get search results & 'login' is the users' name on github
-export const getUser = async (login) => {
+//     const response = await fetch(`${GITHUB_URL}/users/${login}`,
+//         {
+//             headers: {
+//                 Authorization: `token ${GITHUB_TOKEN}`,
+//             },
+//         })
+//     // if user page error
+//     if (response.status === 404) {
+//         window.location = '/notfound'
+//     } else {
+//         const data = await response.json()
 
-    const response = await fetch(`${GITHUB_URL}/users/${login}`,
-        {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`,
-            },
-        })
-    // if user page error
-    if (response.status === 404) {
-        window.location = '/notfound'
-    } else {
-        const data = await response.json()
+//         return data
+//     }
+// }
 
-        return data
-    }
+// // Get user repos
+// export const getUserRepos = async (login) => {
+
+//     // Get the top repos
+//     const params = new URLSearchParams({
+//         sort: 'created',
+//         per_page: 10
+//     })
+//     // ?${params} added to accompany the top repos
+//     const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,
+//         {
+//             headers: {
+//                 Authorization: `token ${GITHUB_TOKEN}`,
+//             },
+//         })
+
+//     const data = await response.json()
+
+//     return data
+// }
+//------------------------------ END REMOVE / REPLACE 
+
+// Get user and repos
+export const getUserAndRepos = async (login) => {
+    const [user, repos] = await Promise.all([
+        github.get(`/users/${login}`),
+        github.get(`/users/${login}/repos`)
+    ])
+
+    return { user: user.data, repos: repos.data }
 }
-
-// Get user repos
-export const getUserRepos = async (login) => {
-
-    // Get the top repos
-    const params = new URLSearchParams({
-        sort: 'created',
-        per_page: 10
-    })
-    // ?${params} added to accompany the top repos
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,
-        {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`,
-            },
-        })
-
-    const data = await response.json()
-
-    return data
-}
-
