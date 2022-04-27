04eae2dc6606451ceaa662a57b49833fe60481ee
Alan Cuenca <alan.r.cuenca@gmail.com>
2022-04-26 21:49:31 -0700

    moved getUsers from context to actions file

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -21,3 +21,51 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
     //return the data
     return items
 }
+
+   // Get search results & 'login' is the users' name on github
+   const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`,
+        {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            },
+        })
+    // if user page error
+    if (response.status === 404) {
+        window.location = '/notfound'
+    } else {
+        const data = await response.json()
+
+        dispatch({
+            type: 'GET_USER', // this action is in reducer
+            payload: data, // data is the single user return
+        })
+    }
+}
+
+    // Get user repos
+const getUserRepos = async (login) => {
+    setLoading()
+    // Get the top repos
+    const params = new URLSearchParams({
+        sort: 'created',
+        per_page: 10
+    })
+    // ?${params} added to accompany the top repos
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, 
+        {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            },
+        })
+
+        const data = await response.json()
+
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data,
+        })
+    }
+
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,53 +16,6 @@ export const GithubProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
  
-    // Get search results & 'login' is the users' name on github
-    const getUser = async (login) => {
-        setLoading()
-
-        const response = await fetch(`${GITHUB_URL}/users/${login}`,
-            {
-                headers: {
-                    Authorization: `token ${GITHUB_TOKEN}`,
-                },
-            })
-        // if user page error
-        if (response.status === 404) {
-            window.location = '/notfound'
-        } else {
-            const data = await response.json()
-
-            dispatch({
-                type: 'GET_USER', // this action is in reducer
-                payload: data, // data is the single user return
-            })
-        }
-    }
-
-        // Get user repos
-    const getUserRepos = async (login) => {
-        setLoading()
-        // Get the top repos
-        const params = new URLSearchParams({
-            sort: 'created',
-            per_page: 10
-        })
-        // ?${params} added to accompany the top repos
-        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, 
-            {
-                headers: {
-                    Authorization: `token ${GITHUB_TOKEN}`,
-                },
-            })
-
-            const data = await response.json()
-
-            dispatch({
-                type: 'GET_REPOS',
-                payload: data,
-            })
-        }
-
     // Clear users from state
     const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
 
