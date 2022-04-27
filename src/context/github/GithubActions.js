8fc82373fc47f082552369e2b512a5a0fa9d13f5
Alan Cuenca <alan.r.cuenca@gmail.com>
2022-04-26 23:27:17 -0700

    downloaded axios and replaced our fetch

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,11 @@
+import axios from "axios"
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+const github = axios.create({
+    baseURL: GITHUB_URL,
+    headers: { Authorization: `token ${GITHUB_TOKEN}` }
+})
 
 // Get search results
 export const searchUsers = async (text) => {
@@ -7,19 +13,9 @@ export const searchUsers = async (text) => {
     const params = new URLSearchParams({
         q: text,
     })
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`,
-        {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`,
-            },
-        })
-    // END fetch
-
-    const { items } = await response.json()
-
-    //return the data
-    return items
+    // Axios replaced out async / await fetch
+    const response = await github.get(`/search/users?${params}`)
+    return response.data.items
 }
 
 // Get search results & 'login' is the users' name on github
